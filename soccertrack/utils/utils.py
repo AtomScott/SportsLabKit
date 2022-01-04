a59aa685c904704888f377e748a2d1fad6a60407
atomscott <atom.james.scott@gmail.com>
2022-01-04 19:05:22 +0900

    add ffmpeg options to make_movie

--- soccertrack/utils/utils.py
+++ soccertrack/utils/utils.py
@@ -1,7 +1,7 @@
 """Genereal utils."""
 import os
 from datetime import datetime
-from typing import Iterable
+from typing import Iterable, Optional
 import cv2 as cv
 import numpy as np
 from numpy.typing import NDArray
@@ -85,19 +85,79 @@ def cv2pil(image: NDArray[np.uint8]) -> Image.Image:
     return new_image
 
 
-def make_video(frames: Iterable[NDArray[np.uint8]], outpath: str) -> None:
+def make_video(
+    frames: Iterable[NDArray[np.uint8]],
+    outpath: str,
+    vcodec: str = "libx264",
+    preset: str = "medium",
+    crf: int = 23,
+    ss: int = 0,
+    t: Optional[int] = None,
+    c: bool = False,
+    scale_filter: Optional[str] = None,
+    input_framerate: int = 30,
+) -> None:
     """Make video from a list of opencv format frames.
 
     Args:
         frames (Iterable[NDArray[np.uint8]]): List of opencv format frames
         outpath (str): Path to output video file
+        vcodec (str): Video codec.
+        preset (str): Video encoding preset. A preset is a collection of options
+            that will provide a certain encoding speed to compression ratio. A
+            slower preset will provide better compression (compression is quality
+            per filesize). Use the slowest preset that you have patience for.
+            The available presets in descending order of speed are:
 
+            - ultrafast
+            - superfast
+            - veryfast
+            - faster
+            - fast
+            - medium (default preset)
+            - slow
+            - slower
+            - veryslow
+
+            Defaults to `medium`.
+
+        crf (int): Constant Rate Factor. Use the crf (Constant Rate Factor)
+            parameter to control the output quality. The lower crf, the higher
+            the quality (range: 0-51). Visually lossless compression corresponds
+            to -crf 18. Use the preset parameter to control the speed of the
+            compression process. Defaults to `23`.
+        ss (int): Start-time of the clip in seconds. Defaults to `0`.
+        t (Optional[int]): Duration of the clip in seconds. Defaults to None.
+        c (bool): copies the first video, audio, and subtitle bitstream from the input to the output file without re-encoding them. Defaults to `False`.
+        scale_fiter (str): Scale filter. Defaults to `None`. If you need to simply resize your video to a specific size (e.g 320×240), `320:240` is the correct value.
+            If you want to keep the aspect ratio, specify only one component, either width or height, and set the other component to -1
+        input_framerate (int): Input framerate. Defaults to `25`.
     Todo:
         * add FPS option
         * functionality to use PIL image
         * reconsider compression (current compression is not good)
     """
-    writer = WriteGear(output_filename=outpath, compression_mode=True)
+
+    output_params = {
+        k: v
+        for k, v in {
+            "-vcodec": vcodec,
+            # encoding quality
+            "-crf": 0,
+            "-preset": "medium",
+            # Trimming
+            "-c": c,
+            "-ss": ss,
+            "-t": t,
+            # frame rate
+            "-input_framerate": input_framerate,
+        }
+        if v is not None
+    }
+
+    writer = WriteGear(
+        output_filename=outpath, compression_mode=True, output_params=output_params
+    )
 
     # loop over
     for frame in tqdm(frames):
@@ -190,6 +250,30 @@ class ImageIterator:
             self._index += 1
             return img
         raise StopIteration
+
+
+def merge_dict_of_lists(d1: dict, d2: dict) -> dict:
+    """Merge two dicts of lists.
+
+    Parameters
+    ----------
+    d1 : dict
+        The first dict to merge.
+    d2 : dict
+        The second dict to merge.
+
+    Returns
+    -------
+    dict
+        The merged dict.
+    """
+    ret = {k: list(v) for k, v in d1.items()}
+    for k, _ in d1.items():
+        if k in d2.keys():
+            ret[k] += d2[k]
+        else:
+            ret[k] = d2[k]
+    return ret
 
 
 # Due to memory consumption concerns, the function below has been replaced by the function that uses vidgear above.
