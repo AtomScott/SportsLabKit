a65ce8fa5bf8e595bceed389118959d6ea15c695
IkumaUchida <ikuma.uchida@gmail.com>
2022-09-13 04:38:34 +0900

    Apply the evaluation index to bbox_df

--- soccertrack/utils/utils.py
+++ soccertrack/utils/utils.py
@@ -12,6 +12,7 @@ from omegaconf import OmegaConf
 from PIL import Image
 from soccertrack.utils import logger, tqdm
 from vidgear.gears import WriteGear
+import pandas as pd
 
 OmegaConf.register_new_resolver(
     "now", lambda x: datetime.now().strftime(x), replace=True
@@ -195,6 +196,40 @@ def make_video(
 
     writer.close()
 
+def df2list(df: pd.DataFrame, xywh=True) -> list:
+    """Convert a dataframe column to a 2-dim list for evaluation of object detection.
+
+    Args:
+        df (pd.DataFrame): Dataframe
+        xywh (bool): If True, convert to x1y1x2y2 format. Defaults to True.
+        
+    Returns:
+        bbox_2dim_list: 2-dim list
+    """
+    bbox_2dim_list = []
+    bbox_cols = ['bb_left', 'bb_top', 'bb_width', 'bb_height', 'conf', 'class_id']
+    num_cols = len(bbox_cols)
+    df2list = df.values.tolist()
+    
+    for frame_id, frame_raw in enumerate(df2list):
+        for idx in range(0,len(frame_raw), num_cols):
+            bbox_2dim_list.append([frame_id] + frame_raw[idx:idx+num_cols])
+    
+    #extract nan rows
+    bbox_2dim_list = [x for x in bbox_2dim_list if not any(pd.isnull(x))]
+    if xywh:
+        for bbox in bbox_2dim_list:
+            bbox[1] = int(bbox[1])
+            bbox[2] = int(bbox[2])
+            bbox[3] = int(bbox[3] + bbox[1])
+            bbox[4] = int(bbox[4] + bbox[2])
+    else:
+        for bbox in bbox_2dim_list:
+            bbox[1] = int(bbox[1])
+            bbox[2] = int(bbox[2])
+            bbox[3] = int(bbox[3])
+            bbox[4] = int(bbox[4])
+    return bbox_2dim_list
 
 class MovieIterator:
     def __init__(self, path: str):
@@ -241,7 +276,6 @@ class MovieIterator:
                 return img
             logger.debug("Unexpected end.")  # <- Not sure why this happens
         raise StopIteration
-
 
 class ImageIterator:
     def __init__(self, path: str):
