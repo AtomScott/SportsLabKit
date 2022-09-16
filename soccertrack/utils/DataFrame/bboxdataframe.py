8339730187a72984e3c2814f55f32a87f5e32ec9
IkumaUchida <ikuma.uchida@gmail.com>
2022-09-17 05:00:14 +0900

    Apply prospector to bboxdataframe

--- soccertrack/utils/DataFrame/bboxdataframe.py
+++ soccertrack/utils/DataFrame/bboxdataframe.py
@@ -1,23 +1,18 @@
-from __future__ import division as _division
-from __future__ import print_function as _print_function
+from __future__ import annotations
+
+import random
+from hashlib import md5
+from typing import Any, Iterator, Optional, Type
+
+import cv2
+import numpy as np
 import pandas as pd
+from tqdm import tqdm
+
+from ..utils import MovieIterator
 from .base import SoccerTrackMixin
 
-
-import os as _os
-import os.path as _path
-import numpy as np
-import cv2 as _cv2
-# import numpy as _np
-from hashlib import md5 as _md5
-from PIL import ImageFont
-import random
-from tqdm import tqdm
-from soccertrack.utils.utils import MovieIterator, make_video
-
-_LOC = _path.realpath(_path.join(_os.getcwd(),_path.dirname(__file__)))
-
-#https://clrs.cc/
+# https://clrs.cc/
 _COLOR_NAME_TO_RGB = dict(
     navy=((0, 38, 63), (119, 193, 250)),
     blue=((0, 120, 210), (173, 220, 252)),
@@ -34,40 +29,31 @@ _COLOR_NAME_TO_RGB = dict(
     purple=((179, 17, 193), (241, 167, 244)),
     black=((24, 24, 24), (220, 220, 220)),
     gray=((168, 168, 168), (0, 0, 0)),
-    silver=((220, 220, 220), (0, 0, 0))
+    silver=((220, 220, 220), (0, 0, 0)),
 )
 
 _COLOR_NAMES = list(_COLOR_NAME_TO_RGB)
 
-# _FONT_PATH = _os.path.join(_LOC, "Ubuntu-B.ttf")
-# _FONT_HEIGHT = 45
-# _FONT = ImageFont.truetype(None, _FONT_HEIGHT)
 
-def _rgb_to_bgr(color):
+def _rgb_to_bgr(color: tuple[int, ...]) -> list[Any]:
+    """Convert RGB color to BGR color.
+
+    Args:
+        color (tuple): RGB color.
+
+    Returns:
+        list: BGR color.
+
+    """
     return list(reversed(color))
 
-# def _color_image(image, font_color, background_color):
-#     return background_color + (font_color - background_color) * image / 255
 
-# def _get_label_image(text, font_color_tuple_bgr, background_color_tuple_bgr):
-#     text_image = _FONT.getmask(text)
-#     shape = list(reversed(text_image.size))
-#     bw_image = np.array(text_image).reshape(shape)
-
-    # image = [
-    #     _color_image(bw_image, font_color, background_color)[None, ...]
-    #     for font_color, background_color
-    #     in zip(font_color_tuple_bgr, background_color_tuple_bgr)
-    # ]
-
-    # return np.concatenate(image).transpose(1, 2, 0)
-
-X1_INDEX = 1
-Y1_INDEX = 2
-X2_INDEX = 3
-Y2_INDEX = 4
-W_INDEX = 3
-H_INDEX = 4
+BB_LEFT_INDEX = 1
+BB_TOP_INDEX = 2
+BB_RIGHT_INDEX = 3
+BB_BOTTOM_INDEX = 4
+BB_WIDTH_INDEX = 3
+BB_HEIGHT_INDEX = 4
 
 color_list = []
 i = 0
@@ -75,144 +61,204 @@ while i < 1000:
     color_list.append(_COLOR_NAMES[random.randint(0, len(_COLOR_NAMES) - 1)])
     i += 1
 
-class BBoxDataFrame(SoccerTrackMixin, pd.DataFrame):
+
+class BBoxDataFrame(SoccerTrackMixin, pd.DataFrame):  # type: ignore
     @property
-    def _constructor(self):
+    def _constructor(self: pd.DataFrame) -> Type[BBoxDataFrame]:
+        """Return the constructor for the DataFrame.
+
+        Args:
+            self (pd.DataFrame): DataFrame object.
+
+        Returns:
+            BBoxDataFrame: BBoxDataFrame object.
+        """
         return BBoxDataFrame
 
-    # @property
-    # def _constructor_sliced(self):
-    #     raise NotImplementedError("This pandas method constructs pandas.Series object, which is not yet implemented in {self.__name__}.")
+        # df: pd.DataFrame
 
-    def to_list(self, df: pd.DataFrame, xywh=True) -> list:
+    def to_list(self: BBoxDataFrame, xywh: bool = True) -> list[list[float]]:
         """Convert a dataframe column to a 2-dim list for evaluation of object detection.
+
         Args:
-            df (pd.DataFrame): Dataframe
+            df (BBoxDataFrame): BBoxDataFrame
             xywh (bool): If True, convert to x1y1x2y2 format. Defaults to True.
-            
+
         Returns:
-            bbox_2dim_list: 2-dim list
+            bbox_2dim_list(list): 2-dim list
+
+        Note:
+            Description of each element in bbox_2dim_list.
+            The elements of the final included bbox_2dim_list are as follows:
+            bbox_cols(list) : ['frame_id', 'bb_left', 'bb_top', 'bb_right', 'bb_bottom','conf', 'class_id' ,'obj_id']
+                frame_id(int) : Frame ID
+                bb_left(float) : Bounding box left coordinate
+                bb_top(float) : Bounding box top coordinate
+                bb_right(float) : Bounding box right coordinate
+                bb_bottom(float) : Bounding box bottom coordinate
+                conf(float) : Confidence score
+                class_id(int) : Class ID. In the normal case, follow the value of the coco image dataset.
+                obj_id(int) : Object ID. This id is a unique integer value for each track
+                            (corresponding to each column in the BBoxDataframe) that is used when evaluating tracking.
+
+            The BBoxDataframe input to this function contains information other than frame_id and obj_id in advance.
+            This function adds the obj_id column and converts the BBoxDataframe to a list.
         """
 
-        #Apply def append_object_id
-        obj_id  = 0
+        # append_object_id
+        obj_id = 0
         id_list = []
-        for column in df.columns:
+        for column in self.columns:
             team_id = column[0]
-            player_id = column[1] 
+            player_id = column[1]
             id_list.append((team_id, player_id))
         for id in sorted(set(id_list)):
-            df.loc[:, (id[0],  id[1], 'obj_id')] = obj_id
+            self.loc[
+                :, (id[0], id[1], "obj_id")
+            ] = obj_id  # Add obj_id column for each player's Attribute
             obj_id += 1
-        df = df[df.sort_index(axis=1,level=[0,1],ascending=[True,True]).columns]
-
+        df_sort = self[
+            self.sort_index(axis=1, level=[0, 1], ascending=[True, True]).columns
+        ]
+        # Create two-dimensional lists from dataframes
         bbox_2dim_list = []
-        bbox_cols = ['bb_left', 'bb_top', 'bb_width', 'bb_height', 'conf','class_id' ,'obj_id']
+        bbox_cols = [
+            "bb_left",
+            "bb_top",
+            "bb_width",
+            "bb_height",
+            "conf",
+            "class_id",
+            "obj_id",
+        ]
         num_cols = len(bbox_cols)
-        df2list = df.values.tolist()
-        
+        df2list = df_sort.values.tolist()
+
         for frame_id, frame_raw in enumerate(df2list):
-            for idx in range(0,len(frame_raw), num_cols):
-                bbox_2dim_list.append([frame_id] + frame_raw[idx:idx+num_cols])
-        
-        #extract nan rows
-        bbox_2dim_list = [x for x in bbox_2dim_list if not any(pd.isnull(x))]
-        # print(bbox_2dim_list)
-        
-        for bbox in bbox_2dim_list:    
+            for idx in range(0, len(frame_raw), num_cols):
+                if not any(
+                    pd.isnull(frame_raw[idx : idx + num_cols])
+                ):  # extract nan rows
+                    bbox_2dim_list.append([frame_id] + frame_raw[idx : idx + num_cols])
+
+        # bbox_2dim_list = [x for x in bbox_2dim_list if not any(pd.isnull(x))]
+
+        for bbox in bbox_2dim_list:
             if xywh:
-                bbox[X1_INDEX] = bbox[X1_INDEX]
-                bbox[Y1_INDEX] = bbox[Y1_INDEX]
-                bbox[X2_INDEX] = bbox[W_INDEX] + bbox[X1_INDEX]
-                bbox[Y2_INDEX] = bbox[H_INDEX] + bbox[Y1_INDEX]
+                bbox[BB_RIGHT_INDEX] = bbox[BB_WIDTH_INDEX] + bbox[BB_LEFT_INDEX]
+                bbox[BB_BOTTOM_INDEX] = bbox[BB_HEIGHT_INDEX] + bbox[BB_TOP_INDEX]
 
         return bbox_2dim_list
 
-    def visualize_frame(self, frame_idx, frame):
+    def visualize_frame(
+        self: BBoxDataFrame, frame_idx: int, frame: np.ndarray
+    ) -> np.ndarray:
+        """Visualize the bounding box of the specified frame.
+
+        Args:
+            self (BBoxDataFrame): BBoxDataFrame object.
+            frame_idx (int): Frame ID.
+            frame (np.ndarray): Frame image.
+
+        Returns:
+            frame(np.ndarray): Frame image with bounding box.
+        """
 
         BB_LEFT_INDEX = 0
         BB_TOP_INDEX = 1
         BB_WIDTH_INDEX = 2
         BB_HEIGHT_INDEX = 3
 
-        # color_list =[]
-        # unique_obj_ids = self.columns.get_level_values('PlayerID').unique()
-        # for obj_id in unique_obj_ids:
-        # color = _COLOR_NAMES[random.randint(0, len(_COLOR_NAMES) - 1)]
-        # print(color_list)
-
-        unique_team_ids = self.columns.get_level_values('TeamID').unique()
+        unique_team_ids = self.columns.get_level_values("TeamID").unique()
         for team_id in unique_team_ids:
-            bboxdf_team = self.xs(team_id, level='TeamID', axis=1)
-            # print('------team_id------', team_id)
+            bboxdf_team = self.xs(team_id, level="TeamID", axis=1)
 
-            unique_team_ids = bboxdf_team.columns.get_level_values('PlayerID').unique()
+            unique_team_ids = bboxdf_team.columns.get_level_values("PlayerID").unique()
             for idx, player_id in enumerate(unique_team_ids):
                 color = color_list[idx]
 
-                bboxdf_player = bboxdf_team.xs(player_id, level='PlayerID', axis=1)
+                bboxdf_player = bboxdf_team.xs(player_id, level="PlayerID", axis=1)
                 bboxdf_player = bboxdf_player.reset_index(drop=True)
                 bbox = bboxdf_player.loc[frame_idx]
-                if np.isnan(bbox[BB_LEFT_INDEX]) and np.isnan(bbox[BB_TOP_INDEX]):
-                    continue
-
-                else:
+                if not np.isnan(bbox[BB_LEFT_INDEX]) and not np.isnan(
+                    bbox[BB_TOP_INDEX]
+                ):
                     bb_left = bbox[BB_LEFT_INDEX]
                     bb_top = bbox[BB_TOP_INDEX]
                     bb_right = bb_left + bbox[BB_WIDTH_INDEX]
                     bb_bottom = bb_top + bbox[BB_HEIGHT_INDEX]
 
-                    x,y,x2,y2 = list([int(bb_left), int(bb_top), int(bb_right), int(bb_bottom)])
-                    frame = add(frame, x, y, x2, y2, label=f'{team_id}_{player_id}', color=color)
-
+                    frame = add(
+                        frame,
+                        int(bb_left),
+                        int(bb_top),
+                        int(bb_right),
+                        int(bb_bottom),
+                        label=f"{team_id}_{player_id}",
+                        color=color,
+                    )
         return frame
 
-    def visualize_bbox(self, video_path:str) -> None:
+    def visualize_bbox(self: BBoxDataFrame, video_path: str) -> Iterator[np.ndarray]:
         """Visualize bounding boxes on a video.
+
         Args:
             video_path (str): Path to the video file.
-        
+
         Returns:
             None
         """
-        ####### How to use（任意のタイミングで削除して頂いて構いません）######
-        # test_movie_path  = '/home/guest/dev_repo/SoccerTrack/x_ignore/test_data/F_20200220_1_0000_0030.mp4'
-        # data_path  = '/home/guest/dev_repo/SoccerTrack/x_ignore/test_data/F_20200220_1_0000_0030.csv'
-        # save_dir = './test_data/'
-
-        # bboxdf = load_df(data_path)
-        # img_ = bboxdf.visualize_bbox(test_movie_path)
-
-        # save_path = os.path.join(save_dir, 'bbox_.mp4')
-        # make_video(img_, save_path) ###make_video関数に、ジェネレータを渡す
-
         movie_iterator = MovieIterator(video_path)
-        for frame_idx ,frame in tqdm(enumerate(movie_iterator)):
+        for frame_idx, frame in tqdm(enumerate(movie_iterator)):
             img_ = self.visualize_frame(frame_idx, frame)
             yield img_
 
-def add(image, left, top, right, bottom, label=None, color=None):
+
+def add(
+    img: np.ndarray,
+    left: int,
+    top: int,
+    right: int,
+    bottom: int,
+    label: Optional[str] = None,
+    color: Optional[str] = None,
+) -> np.ndarray:
+    """Add bounding box and label to image.
+
+    Args:
+        img (np.ndarray): Image.
+        left (int): Bounding box left coordinate.
+        top (int): Bounding box top coordinate.
+        right (int): Bounding box right coordinate.
+        bottom (int): Bounding box bottom coordinate.
+        label (str): Label.
+        color (str): Color.
+
+    Returns:
+        img (np.ndarray): Image with bounding box and label.
+    """
+
     _DEFAULT_COLOR_NAME = "purple"
 
-    if type(image) is not np.ndarray:
+    if not isinstance(img, np.ndarray):
         raise TypeError("'image' parameter must be a numpy.ndarray")
     try:
         left, top, right, bottom = int(left), int(top), int(right), int(bottom)
-    except ValueError:
-        raise TypeError("'left', 'top', 'right' & 'bottom' must be a number")
+    except ValueError as e:
+        raise TypeError("'left', 'top', 'right' & 'bottom' must be a number") from e
 
-    if label and type(label) is not str:
+    if label and not isinstance(label, str):
         raise TypeError("'label' must be a str")
 
     if label and not color:
-        hex_digest = _md5(label.encode()).hexdigest()
+        hex_digest = md5(label.encode()).hexdigest()
         color_index = int(hex_digest, 16) % len(_COLOR_NAME_TO_RGB)
         color = _COLOR_NAMES[color_index]
 
     if not color:
         color = _DEFAULT_COLOR_NAME
 
-    if type(color) is not str:
+    if not isinstance(color, str):
         raise TypeError("'color' must be a str")
 
     if color not in _COLOR_NAME_TO_RGB:
@@ -220,20 +266,20 @@ def add(image, left, top, right, bottom, label=None, color=None):
         raise ValueError(msg)
 
     colors = [_rgb_to_bgr(item) for item in _COLOR_NAME_TO_RGB[color]]
-    # print(colors)
-    color, color_text = colors
-    # print((left, top), (right, bottom))
+    color_value, _ = colors
 
-    image = _cv2.rectangle(image, (left, top), (right, bottom), color, 2)
+    img = cv2.rectangle(img, (left, top), (right, bottom), color, 2)
 
     if label:
 
-        _, image_width, _ = image.shape
-        fontface = _cv2.FONT_HERSHEY_TRIPLEX  # フォントの種類
-        fontscale = 0.5  # 文字のスケール
-        thickness = 1  # 文字の太さ
+        _, image_width, _ = img.shape
+        fontface = cv2.FONT_HERSHEY_TRIPLEX
+        fontscale = 0.5
+        thickness = 1
 
-        (label_width, label_height), baseline = _cv2.getTextSize(label, fontface, fontscale, thickness)
+        (label_width, label_height), _ = cv2.getTextSize(
+            label, fontface, fontscale, thickness
+        )
         rectangle_height, rectangle_width = 1 + label_height, 1 + label_width
 
         rectangle_bottom = top
@@ -252,21 +298,23 @@ def add(image, left, top, right, bottom, label=None, color=None):
 
         label_left = rectangle_left + 1
         label_bottom = label_top + label_height
-        label_right = label_left + label_width
+        # label_right = label_left + label_width
 
         rec_left_top = (rectangle_left, rectangle_top)
         rec_right_bottom = (rectangle_right, rectangle_bottom)
 
-        _cv2.rectangle(image, rec_left_top, rec_right_bottom, color, -1)
+        cv2.rectangle(img, rec_left_top, rec_right_bottom, color_value, -1)
 
         # image[label_top:label_bottom, label_left:label_right, :] = label
 
-        _cv2.putText(image,
-            text = label,
+        cv2.putText(
+            img,
+            text=label,
             org=(label_left, int((label_bottom))),
-            fontFace = fontface,
+            fontFace=fontface,
             fontScale=fontscale,
             color=(0, 0, 0),
-            thickness = thickness,
-            lineType=_cv2.LINE_4)
-    return image
+            thickness=thickness,
+            lineType=cv2.LINE_4,
+        )
+    return img
