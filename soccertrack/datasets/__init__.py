9761f73cb9757c2c203f6e9fc852d1d754f251b1
Atom Scott <atom.james.scott@gmail.com>
2022-10-09 17:37:06 +0900

    feat: :sparkles: Easier way to access downloaded dataset

--- soccertrack/datasets/__init__.py
+++ soccertrack/datasets/__init__.py
@@ -1,40 +1,60 @@
 import os
 
 from soccertrack.logging import logger
-
+from typing import Optional
+from pathlib import Path
 from .downloader import KaggleDownloader
 
 __all__ = ["available", "get_path", "KaggleDownloader"]
 
-_module_path = os.path.dirname(__file__)
-_available_dir = [p for p in next(os.walk(_module_path))[1] if not p.startswith("__")]
-_available_csv = {"soccertrack sample": "soccertrack_sample.csv"}
-_available_mp4 = {
-    "soccertrack sample": "https://drive.google.com/file/d/1Vxc1NXwLiD3T6cqmlbjgjr-9umDty5Va/view?usp=sharing"
+_module_path = Path(__file__).parent
+_available_dir = {
+    'GNSS': _module_path / 'GNSS',
+    'top-view': _module_path / 'top-view',
+    'wide-view': _module_path / 'wide-view',
 }
-available = _available_dir + list(_available_csv.keys())
 
+for d, path in _available_dir.items():
+    if not path.exists():
+        _available_dir.pop(d)
+    
+_available_files = {
+    'drone_keypoints': _module_path / 'drone_keypoints.json',
+    'fisheye_keypoints': _module_path / 'fisheye_keypoints.json',
+    'gnss_keypoints': _module_path / 'gnss_keypoints.json'
+}
 
-def get_path(dataset: str, type: str = "csv") -> str:
+for d, path in _available_files.items():
+    if not path.exists:
+        _available_files.pop(d), print(f"Dataset {d} not available")
+
+available = list(_available_dir.keys()) + list(_available_files.keys())
+
+def get_path(dataset:Optional[str]=None) -> str:
     """Get the path to the data file.
 
     Args:
-        dataset (str): Name of the dataset. See `soccertrack.datasets.available` for all options.
-        dataset_type (str): Type of the dataset. Either 'csv' or 'mp4'.
+        dataset (str): Name of the dataset. If None, print the available datasets.
 
     Returns:
         str: Path to the data file.
     """
-    if type == "csv":
-        if dataset in _available_csv:
-            fpath = os.path.abspath(os.path.join(_module_path, _available_csv[dataset]))
-            return fpath
-    if type == "mp4":
-        if dataset in _available_mp4:
-            fpath = _available_mp4[dataset]
-            logger.info(f"Download the dataset from {fpath}")
-            return fpath
+    
+    if dataset is None:
+        print("Available keys:")
+        for d in available: 
+            print(f" - {d}")
+        return
 
+    if dataset in _available_dir:
+        return _available_dir[dataset]
+    
+    if dataset.split('/')[0] in _available_dir:
+        ret_path = _available_dir[dataset.split('/')[0]] / dataset.split('/')[1]
+        assert ret_path.exists(), f"File {ret_path} not available"
+        return ret_path
+    
     msg = f"The dataset '{dataset}' is not available. "
     msg += f"Available datasets are {', '.join(available)}"
     raise ValueError(msg)
+
