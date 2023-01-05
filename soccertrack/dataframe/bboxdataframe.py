7d2ec0eeef0f2d72a03aef58e120fb3ee854913c
Atom Scott <atom.james.scott@gmail.com>
2023-01-06 00:12:21 +0900

    Add to_list_of_tuples_format

--- soccertrack/dataframe/bboxdataframe.py
+++ soccertrack/dataframe/bboxdataframe.py
@@ -201,14 +201,14 @@ class BBoxDataFrame(SoccerTrackMixin, pd.DataFrame):
             list: list of shape (N, M, 5) in YOLOv5 format. Where N is the number of frames, M is the number of objects in the frame, and 5 is the number of attributes (class_id, x_center, y_center, width, height).
         """
 
-        if mapping is None:
-            mapping = {"TeamID": {'3': 1}, "PlayerID": {}}
-
         if save_dir is not None:
             save_dir = Path(save_dir)
             save_dir.mkdir(parents=True, exist_ok=True)
 
         df = self.to_long_df().reset_index()
+
+        if mapping is None:
+            mapping = {"TeamID": {"3": 1}, "PlayerID": {}}
 
         team_mappings = df["TeamID"].map(mapping["TeamID"])
         player_mappings = df["PlayerID"].map(mapping["PlayerID"])
@@ -241,6 +241,51 @@ class BBoxDataFrame(SoccerTrackMixin, pd.DataFrame):
             pd.DataFrame: Dataframe in MOT format.
         """
         raise NotImplementedError
+
+    def to_list_of_tuples_format(
+        self,
+        mapping: dict[dict[Any, Any], dict[Any, Any]] = None,
+        na_class: int | str = "player",
+    ):  # TODO: Add docstring
+        """Convert a dataframe to a list of tuples.
+
+        Converts a dataframe to a list of tuples necessary for calculating object detection metrics such as mAP and AP scores. The specification for each list element is as follows:
+        (x, y, w, h, confidence, class_id, image_name, object_id)
+
+        Returns:
+            list: List of tuples.
+        """
+        long_df = self.to_long_df().reset_index()
+
+        # TODO: Abstract mapping functionality to a separate function
+
+        if mapping is None:
+            mapping = {"TeamID": {"3": "ball"}, "PlayerID": {}}
+
+        team_mappings = long_df["TeamID"].map(mapping["TeamID"])
+        player_mappings = long_df["PlayerID"].map(mapping["PlayerID"])
+        long_df["class"] = player_mappings.combine_first(team_mappings).fillna(na_class)
+        long_df["image_name"] = long_df["frame"].astype(str)
+        long_df["object_id"] = long_df["PlayerID"] + "_" + long_df["TeamID"]
+
+        # create a unique object id for each object in ascending order
+        assigned_ids = {
+            p_id_t_id: object_id
+            for object_id, p_id_t_id in enumerate(long_df["object_id"].unique())
+        }
+        long_df["object_id"] = long_df["object_id"].map(assigned_ids)
+
+        cols = [
+            "bb_left",
+            "bb_top",
+            "bb_width",
+            "bb_height",
+            "conf",
+            "class",
+            "image_name",
+            "object_id",
+        ]
+        return long_df[cols].values
 
 
 def add_bbox_to_frame(
