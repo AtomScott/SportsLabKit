b519fc25c256177e790a845b8437fee1ac824278
Atom Scott <atom.james.scott@gmail.com>
2023-01-09 20:21:29 +0900

    add drop option to iteration methods

--- soccertrack/dataframe/base.py
+++ soccertrack/dataframe/base.py
@@ -36,40 +36,52 @@ class SoccerTrackMixin(object):
         for index, group in self.groupby("frame"):
             yield index, apply_func(group)
 
-    def iter_players(self, apply_func=None):
+    def iter_players(self, apply_func=None, drop=True):
         """Iterate over the players of the dataframe.
 
         Args:
             apply_func (function, optional): Function to apply to each group. Defaults to None.
+            drop (bool, optional): Drop the level of the dataframe. Defaults to True.
         """
         if apply_func is None:
             apply_func = lambda x: x
         for index, group in self.groupby(level=("TeamID", "PlayerID"), axis=1):
-            yield index, apply_func(
-                group.droplevel(level=("TeamID", "PlayerID"), axis=1)
-            )
+            if drop:
+                yield index, apply_func(
+                    group.droplevel(level=("TeamID", "PlayerID"), axis=1)
+                )
+            else:
+                yield index, apply_func(group)
 
-    def iter_teams(self, apply_func=None):
+    def iter_teams(self, apply_func=None, drop=True):
         """Iterate over the teams of the dataframe.
 
         Args:
             apply_func (function, optional): Function to apply to each group. Defaults to None.
+            drop (bool, optional): Drop the level of the dataframe. Defaults to True.
         """
         if apply_func is None:
             apply_func = lambda x: x
         for index, group in self.groupby(level="TeamID", axis=1):
-            yield index, apply_func(group.droplevel(level=("TeamID"), axis=1))
+            if drop:
+                yield index, apply_func(group.droplevel(level=("TeamID"), axis=1))
+            else:
+                yield index, apply_func(group)
 
-    def iter_attributes(self, apply_func=None):
+    def iter_attributes(self, apply_func=None, drop=True):
         """Iterate over the attributes of the dataframe.
 
         Args:
             apply_func (function, optional): Function to apply to each group. Defaults to None.
+            drop (bool, optional): Drop the level of the dataframe. Defaults to True.
         """
         if apply_func is None:
             apply_func = lambda x: x
         for index, group in self.groupby(level="Attributes", axis=1):
-            yield index, apply_func(group.droplevel(level=("Attributes"), axis=1))
+            if drop:
+                yield index, apply_func(group.droplevel(level=("Attributes"), axis=1))
+            else:
+                yield index, group
 
     def to_long_df(self, level="Attributes"):
         """Convert a dataframe to a long format.
