4c897e135656c68a7eed94fad5a6ba67804b24b7
Atom Scott <atom.james.scott@gmail.com>
2023-01-15 08:46:01 +0900

    add CostMatrixMetrics

--- soccertrack/metrics/__init__.py
+++ soccertrack/metrics/__init__.py
@@ -1,10 +1,29 @@
-from .hota import hota_score
-from .identity import identity_score
-from .mota import mota_score
-from .object_detection import (
+from soccertrack.metrics.hota import hota_score
+from soccertrack.metrics.identity import identity_score
+from soccertrack.metrics.mota import mota_score
+from soccertrack.metrics.object_detection import (
     ap_score,
     ap_score_range,
     iou_score,
     map_score,
     map_score_range,
 )
+from soccertrack.metrics.cost_matrix_metrics import (
+    BaseCostMatrixMetric,
+    IoUCMM,
+    CosineCMM,
+)
+
+__all__ = [
+    "hota_score",
+    "identity_score",
+    "mota_score",
+    "ap_score",
+    "ap_score_range",
+    "iou_score",
+    "map_score",
+    "map_score_range",
+    "BaseCostMatrixMetric",
+    "IoUCMM",
+    "CosineCMM",
+]
---
+++ soccertrack/metrics/cost_matrix_metrics.py
@@ -0,0 +1,88 @@
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from typing import Sequence
+
+import numpy as np
+from scipy.spatial.distance import cdist
+
+from soccertrack.metrics import iou_score
+from soccertrack.tracking_model import SingleObjectTracker
+from soccertrack.types import Detection
+from soccertrack.checks import (
+    _check_trackers,
+    _check_detections,
+    _check_cost_matrix,
+)
+
+
+class BaseCostMatrixMetric(ABC):
+    """A base class for computing the cost matrix between trackers and
+    detections."""
+
+    def __call__(
+        self, trackers: Sequence[SingleObjectTracker], detections: Sequence[Detection]
+    ) -> np.ndarray:
+        """Calculate the metric between trackers and detections.
+
+        Args:
+            trackers: A list of trackers.
+            detections: A list of detections.
+
+        returns:
+            An array of containing the metric between trackers and detections.
+        """
+        _check_trackers(trackers)
+        _check_detections(detections)
+
+        cost_matrix = self.compute_metric(trackers, detections)
+        _check_cost_matrix(cost_matrix, trackers, detections)
+        return cost_matrix
+
+    @abstractmethod
+    def compute_metric(
+        self, trackers: Sequence[SingleObjectTracker], detections: Sequence[Detection]
+    ) -> np.ndarray:
+        """Calculate the metric between trackers and detections.
+
+        Args:
+            trackers: A list of trackers.
+            detections: A list of detections.
+
+        returns:
+            An array of containing the metric between trackers and detections.
+        """
+        raise NotImplementedError
+
+
+class IoUCMM(BaseCostMatrixMetric):
+    """Compute the IoU Cost Matrix Metric between trackers and detections."""
+
+    def compute_metric(
+        self, trackers: Sequence[SingleObjectTracker], detections: Sequence[Detection]
+    ) -> np.ndarray:
+        bb1 = np.array(
+            [
+                (t.box[0], t.box[1], t.box[0] + t.box[2], t.box[1] + t.box[3])
+                for t in trackers
+            ]
+        )
+        bb2 = np.array(
+            [
+                (d.box[0], d.box[1], d.box[0] + d.box[2], d.box[1] + d.box[3])
+                for d in detections
+            ]
+        )
+        return 1 - cdist(bb1, bb2, iou_score)
+
+
+class CosineCMM(BaseCostMatrixMetric):
+    """Compute the Cosine Cost Matrix Metric between trackers and
+    detections."""
+
+    def compute_metric(
+        self, trackers: Sequence[SingleObjectTracker], detections: Sequence[Detection]
+    ) -> np.ndarray:
+        vectors1 = np.array([t.feature for t in trackers])
+        vectors2 = np.array([d.feature for d in detections])
+        return cdist(vectors1, vectors2, "cosine")
