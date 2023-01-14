50098c7c7fbc2fc4df07ab582ab23bc8c509ca74
Atom Scott <atom.james.scott@gmail.com>
2023-01-15 08:44:54 +0900

    add matching functions

--- soccertrack/tracking_model/matching.py
+++ soccertrack/tracking_model/matching.py
@@ -1,144 +1,63 @@
-""" assignment cost calculation & matching methods """
+"""assignment cost calculation & matching methods."""
+
+from __future__ import annotations
 
 from abc import ABC, abstractmethod
 from typing import Any, Callable, Optional, Sequence, Tuple
 
 import numpy as np
 import scipy
+from scipy.optimize import linear_sum_assignment
 from scipy.spatial.distance import cdist
+
+from soccertrack.checks import (_check_cost_matrix, _check_detections,
+                                _check_matches, _check_trackers)
+from soccertrack.metrics import (BaseCostMatrixMetric, CosineCMM, IoUCMM,
+                                 iou_score)
+from soccertrack.tracking_model import SingleObjectTracker
+from soccertrack.types import Detection
 
 EPS = 1e-7
 
 
-def calculate_iou(bboxes1, bboxes2, dim: int = 2):
-    """expected bboxes size: (-1, 2*dim)"""
-    bboxes1 = np.array(bboxes1).reshape((-1, dim * 2))
-    bboxes2 = np.array(bboxes2).reshape((-1, dim * 2))
-
-    coords_b1 = np.split(bboxes1, 2 * dim, axis=1)
-    coords_b2 = np.split(bboxes2, 2 * dim, axis=1)
-
-    coords = np.zeros(shape=(2, dim, bboxes1.shape[0], bboxes2.shape[0]))
-    val_inter, val_b1, val_b2 = 1.0, 1.0, 1.0
-    for d in range(dim):
-        coords[0, d] = np.maximum(coords_b1[d], np.transpose(coords_b2[d]))  # top-left
-        coords[1, d] = np.minimum(
-            coords_b1[d + dim], np.transpose(coords_b2[d + dim])
-        )  # bottom-right
-
-        val_inter *= np.maximum(coords[1, d] - coords[0, d], 0)
-        val_b1 *= coords_b1[d + dim] - coords_b1[d]
-        val_b2 *= coords_b2[d + dim] - coords_b2[d]
-
-    iou = val_inter / (
-        np.clip(val_b1 + np.transpose(val_b2) - val_inter, a_min=0, a_max=None) + EPS
-    )
-    return iou
-
-
-def angular_similarity(vectors1, vectors2):
-    sim = 1 - cdist(vectors1, vectors2, "cosine") / 2  # kept in range <0,1>
-    return sim
-
-
-def _sequence_has_none(seq: Sequence[Any]) -> bool:
-    return any([r is None for r in seq])
-
-
-def cost_matrix_iou_feature(
-    trackers: Sequence[Any],
-    detections: Sequence[Any],
-    feature_similarity_fn=angular_similarity,
-    feature_similarity_beta: float = None,
+def linear_sum_assignment_with_inf(
+    cost_matrix: np.ndarray,
 ) -> Tuple[np.ndarray, np.ndarray]:
+    cost_matrix = np.asarray(cost_matrix)
+    min_inf = np.isneginf(cost_matrix).any()
+    max_inf = np.isposinf(cost_matrix).any()
 
-    # boxes
-    b1 = np.array([t.box() for t in trackers])
-    b2 = np.array([d.box for d in detections])
+    if min_inf and max_inf:
+        raise ValueError("matrix contains both inf and -inf")
 
-    # box iou
-    inferred_dim = int(len(b1[0]) / 2)
-    iou_mat = calculate_iou(b1, b2, dim=inferred_dim)
+    if min_inf or max_inf:
+        values = cost_matrix[~np.isinf(cost_matrix)]
+        if values.size == 0:
+            raise ValueError("matrix contains only inf or -inf")
 
-    # feature similarity
-    if feature_similarity_beta is not None:
-        # get features
-        f1 = [t.feature for t in trackers]
-        f2 = [d.feature for d in detections]
+        m = values.min()
+        M = values.max()
+        n = min(cost_matrix.shape)
+        positive = n * (M - m + np.abs(M) + np.abs(m) + 1)
+        if max_inf:
+            place_holder = (M + (n - 1) * (M - m)) + positive
+        if min_inf:
+            place_holder = (m + (n - 1) * (m - M)) - positive
 
-        if _sequence_has_none(f1) or _sequence_has_none(f2):
-            # fallback to pure IOU due to missing features
-            apt_mat = iou_mat
-        else:
-            sim_mat = feature_similarity_fn(f1, f2)
-            sim_mat = feature_similarity_beta + (1 - feature_similarity_beta) * sim_mat
-
-            # combined aptitude
-            apt_mat = np.multiply(iou_mat, sim_mat)
-    else:
-        apt_mat = iou_mat
-
-    cost_mat = -1.0 * apt_mat
-    return cost_mat, iou_mat
-
-
-def cost_matrix_distance(trackers, detections, dim: int = 2):
-    if len(trackers) == 0 or len(detections) == 0:
-        return np.array([])
-
-    bboxes1 = np.array([t.box() for t in trackers])
-    bboxes2 = np.array([d.box for d in detections])
-
-    centers1 = np.array([b.reshape(2, 2).mean(0) for b in bboxes1])
-    centers2 = np.array([b.reshape(2, 2).mean(0) for b in bboxes2])
-
-    return cdist(centers1, centers2)
-
-
-def match_by_cost_matrix(
-    cost_mat,
-    max_cost: float = 0.1,
-) -> np.ndarray:
-    if len(cost_mat) == 0:
-        return []
-
-    row_ind, col_ind = scipy.optimize.linear_sum_assignment(cost_mat)
-
-    matches = []
-    for r, c in zip(row_ind, col_ind):
-        # check linear assignment winner
-        if cost_mat[r, c] <= max_cost:
-            matches.append((r, c))
-
-    return np.array(matches)
-
-
-def match_by_affinity_matrix(
-    affinity_mat,
-    min_affinity: float = 0.1,
-) -> np.ndarray:
-    if len(affinity_mat) == 0:
-        return []
-
-    row_ind, col_ind = scipy.optimize.linear_sum_assignment(-affinity_mat)
-
-    matches = []
-    for r, c in zip(row_ind, col_ind):
-        # check linear assignment winner
-        if affinity_mat[r, c] >= min_affinity:
-            matches.append((r, c))
-
-    return np.array(matches)
+        cost_matrix[np.isinf(cost_matrix)] = place_holder
+    return linear_sum_assignment(cost_matrix)
 
 
 class BaseMatchingFunction(ABC):
-    """
-    A base class for matching functions.
+    """A base class for matching functions.
+
+    A matching function takes a list of trackers and a list of
+    detections and returns a list of matches. Subclasses should
+    implement the :meth:`compute_cost_matrix` method.
     """
 
-    @abstractmethod
     def __call__(
-        self, trackers: Sequence[Any], detections: Sequence[Any]
+        self, trackers: Sequence[SingleObjectTracker], detections: Sequence[Detection]
     ) -> np.ndarray:
         """Calculate the matching cost between trackers and detections.
 
@@ -149,54 +68,102 @@ class BaseMatchingFunction(ABC):
         returns:
             An array of containing indices of matching pairs of trackers and detections.
         """
-        raise NotImplementedError()
+        _check_trackers(trackers)
+        _check_detections(detections)
 
+        cost_matrix = self.compute_cost_matrix(trackers, detections)
+        _check_cost_matrix(cost_matrix, trackers, detections)
 
-class IOUAndFeatureMatchingFunction(BaseMatchingFunction):
-    """class implements the basic matching function, taking into account
-    detection boxes overlap measured using IOU metric and optional
-    feature similarity measured with a specified metric"""
+        matches = self.match_cost_matrix(cost_matrix)
+        _check_matches(matches, trackers, detections)
 
-    def __init__(
-        self,
-        min_iou: float = 0.1,
-        multi_match_min_iou: float = 1.0 + EPS,
-        feature_similarity_fn: Callable = angular_similarity,
-        feature_similarity_beta: Optional[float] = None,
-    ) -> None:
-        self.min_iou = min_iou
-        self.multi_match_min_iou = multi_match_min_iou
-        self.feature_similarity_fn = feature_similarity_fn
-        self.feature_similarity_beta = feature_similarity_beta
+        return matches
 
-    def __call__(
-        self, trackers: Sequence[Any], detections: Sequence[Any]
+    @abstractmethod
+    def compute_cost_matrix(
+        self, trackers: Sequence[SingleObjectTracker], detections: Sequence[Detection]
     ) -> np.ndarray:
-        return match_by_cost_matrix(
-            trackers,
-            detections,
-            min_iou=self.min_iou,
-            multi_match_min_iou=self.multi_match_min_iou,
-            feature_similarity_fn=self.feature_similarity_fn,
-            feature_similarity_beta=self.feature_similarity_beta,
-        )
+        """Calculate the matching cost between trackers and detections.
+
+        Args:
+            trackers: A list of trackers.
+            detections: A list of detections.
+
+        returns:
+            A 2D numpy array of matching costs between trackers and detections.
+        """
+        pass
+
+    def match_cost_matrix(self, cost_matrix: np.ndarray) -> np.ndarray:
+        """Match trackers and detections based on a cost matrix.
+
+        While this method implements a hungarian algorithm, it is can be
+        overriden by subclasses that implement different matching strategies.
+        Args:
+            cost_matrix: A 2D numpy array of matching costs between trackers and detections.
+
+        returns:
+            A 2D numpy array of shape (n, 2) containing indices of matching pairs of trackers and detections.
+        """
+        matches = np.array(linear_sum_assignment_with_inf(cost_matrix)).T
+        return matches
 
 
-class EuclideanDistance(BaseMatchingFunction):
+class MotionVisualMatchingFunction(BaseMatchingFunction):
+    """A matching function that uses a combination of motion and visual
+    metrics.
+
+    Args:
+        motion_metric: A motion metric. Defaults to `IoUCMM`.
+        motion_metric_beta: The weight of the motion metric. Defaults to 1.
+        motion_metric_gate: The gate of the motion metric, i.e. if the
+            motion metric is larger than this value, the cost will be
+            set to infinity. Defaults to `np.inf`.
+        visual_metric: A visual metric. Defaults to `CosineCMM`.
+        visual_metric_beta: The weight of the visual metric. Defaults to 1.
+        visual_metric_gate: The gate of the visual metric, i.e. if the
+            visual metric is larger than this value, the cost will be
+            set to infinity. Defaults to `np.inf`.
+
+    Note:
+        To implement your own matching function, you can inherit from `BaseMatchingFunction`
+        and override the :meth:`compute_cost_matrix` method.
+    """
+
     def __init__(
         self,
-        max_cost: float = 1000.0,
-        conf_threshold_1: float = 0.5,
-        conf_threshold_2: float = 0.01,
-        multi_match_max_dist: float = 1.0 + EPS,
+        motion_metric: BaseCostMatrixMetric = IoUCMM(),
+        motion_metric_beta: float = 1,
+        motion_metric_gate: float = np.inf,
+        visual_metric: BaseCostMatrixMetric = CosineCMM(),
+        visual_metric_beta: float = 1,
+        visual_metric_gate: float = np.inf,
     ) -> None:
-        self.max_cost = max_cost
-        self.conf_threshold_1 = conf_threshold_1
-        # self.conf_threshold_2
+        self.motion_metric = motion_metric
+        self.motion_metric_beta = motion_metric_beta
+        self.motion_metric_gate = motion_metric_gate
+        self.visual_metric = visual_metric
+        self.visual_metric_beta = visual_metric_beta
+        self.visual_metric_gate = visual_metric_gate
 
-    def __call__(self, trackers, detections) -> np.ndarray:
-        cost_mat = cost_matrix_distance(trackers, detections)
-        return match_by_cost_matrix(
-            cost_mat,
-            max_cost=self.max_cost,
-        )
+    def compute_cost_matrix(
+        self, trackers: Sequence[SingleObjectTracker], detections: Sequence[Detection]
+    ) -> np.ndarray:
+        if len(trackers) == 0 or len(detections) == 0:
+            return np.array([])
+
+        # Compute motion cost
+        motion_cost = self.motion_metric_beta * self.motion_metric(trackers, detections)
+
+        # Gate elements of motion cost matrix to infinity
+        motion_cost[motion_cost > self.motion_metric_gate] = np.inf
+
+        # Compute visual cost
+        visual_cost = self.visual_metric_beta * self.visual_metric(trackers, detections)
+
+        # Gate elements of visual cost matrix to infinity
+        visual_cost[visual_cost > self.visual_metric_gate] = np.inf
+
+        # Compute total cost
+        cost_matrix = motion_cost + visual_cost
+        return cost_matrix
---
+++ tests/test_tracking_model/test_matching.py
@@ -0,0 +1,66 @@
+import os
+import sys
+from importlib.machinery import SourceFileLoader
+from importlib.util import module_from_spec, spec_from_loader
+from pathlib import Path
+from tempfile import TemporaryDirectory
+
+import pandas as pd
+
+TEST_DIR = os.path.dirname(os.path.abspath(__file__))
+PROJECT_DIR = os.path.abspath(os.path.join(TEST_DIR, os.pardir))
+sys.path.insert(0, PROJECT_DIR)
+
+import unittest
+
+import numpy as np
+
+from soccertrack.logger import *
+from soccertrack.tracking_model import (MotionVisualMatchingFunction,
+                                        SingleObjectTracker)
+from soccertrack.types import Detection
+
+dets = [
+    Detection([10, 10, 5, 5], 0.9, 0, [1, 2, 3]),
+    Detection([5, 10, 10, 10], 0.9, 0, [1, 1, 0]),
+    Detection([15, 10, 10, 10], 0.9, 0, [1, 10, 0]),
+]
+
+_det0 = Detection([5, 5, 15, 15], 0.45, 0, [0, 3, 3])
+_det1 = Detection([10, 10, 5, 5], 0.9, 0, [1, 2, 3])
+
+trackers = []
+for det in [_det0, _det1]:
+    sot = SingleObjectTracker()
+    sot.update(detection=det)
+    trackers.append(sot)
+
+
+class TestMatchingFunction(unittest.TestCase):
+    def test_MotionVisualMatchingFunction_no_gates(self):
+        matching_fn = MotionVisualMatchingFunction(
+            motion_metric_gate=np.inf, visual_metric_gate=np.inf
+        )
+        matches = matching_fn(trackers=trackers, detections=dets)
+
+        self.assertEqual(type(matches), np.ndarray)
+        self.assertEqual(matches.shape, (2, 2))
+
+        matches = matches.tolist()
+
+        self.assertListEqual(matches[0], [0, 1])
+        self.assertListEqual(matches[1], [1, 0])
+
+        # # Calculate IOU cost matrix
+        # bboxes1 = np.array([t.box for t in trackers])
+        # bboxes2 = np.array([d.box for d in detections])
+        # iou_matrix = 1 - cdist(bboxes1, bboxes2, calculate_iou)
+        # if self.feature_similarity_beta is None:
+        #     return iou_matrix
+
+        # # Calculate feature cost matrix
+        # features1 = np.array([t.feature for t in trackers])
+        # features2 = np.array([d.feature for d in detections])
+        # feature_matrix = cdist(features1, features2, self.feature_similarity_fn)
+
+        # return iou_matrix + self.feature_similarity_beta * feature_matrix
