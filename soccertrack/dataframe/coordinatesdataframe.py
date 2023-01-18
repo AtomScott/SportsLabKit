29f46332847d988dc975b0eb0e8f621d94e6cff2
Atom Scott <atom.james.scott@gmail.com>
2023-01-18 23:58:10 +0900

    fix kwargs bug and add more docs

--- soccertrack/dataframe/coordinatesdataframe.py
+++ soccertrack/dataframe/coordinatesdataframe.py
@@ -17,6 +17,18 @@ from soccertrack.logger import logger
 from soccertrack.types import _pathlike
 
 
+def merge_dicts(*dicts):
+    """Merge dictionaries.
+
+    Later dictionaries take precedence.
+    """
+    merged = {}
+    for d in dicts:
+        if d is not None:
+            merged.update(d)
+    return merged
+
+
 class CoordinatesDataFrame(SoccerTrackMixin, pd.DataFrame):
 
     _metadata = [
@@ -141,12 +153,14 @@ class CoordinatesDataFrame(SoccerTrackMixin, pd.DataFrame):
 
         Visualize a frame given a frame number and save it to a path. The `CoordinatesDataFrame` is expected to already have been normalized so that the pitch is 105x68, e.g. coordinates on the x-axis range from 0 to 105 and coordinates on the y-axis range from 0 to 68.
 
+        Similarly, you can pass keyword arguments to change the appearance of the markers. For example, to change the size of the markers, you can pass `ms=6` to `away_kwargs` by, e.g. `codf.visualize_frames("animation.gif", away_kwargs={"ms": 6})`. See the `matplotlib.pyplot.plot` documentation for more information. Note that `marker_kwargs` will be used for all markers but will be overwritten by `ball_kwargs`, `home_kwargs` and `away_kwargs` if a dictionary with the same key is passed (later dictionaries take precedence).
+
         Args:
             frame_idx: Frame number.
             save_path: Path to save the image. Defaults to None.
-            ball_key: Key for the ball. Defaults to "ball".
-            home_key: Key for the home team. Defaults to "0".
-            away_key: Key for the away team. Defaults to "1".
+            ball_key: Key (TeamID) for the ball. Defaults to "ball".
+            home_key: Key (TeamID) for the home team. Defaults to "0".
+            away_key: Key (TeamID) for the away team. Defaults to "1".
             marker_kwargs: Keyword arguments for the markers.
             ball_kwargs: Keyword arguments specifically for the ball marker.
             home_kwargs: Keyword arguments specifically for the home team markers.
@@ -156,6 +170,9 @@ class CoordinatesDataFrame(SoccerTrackMixin, pd.DataFrame):
         Note:
             `marker_kwargs` will be used for all markers but will be overwritten by `ball_kwargs`, `home_kwargs` and `away_kwargs`. All keyword arguments are passed to `plt.plot`. `save_kwargs` are passed to `plt.savefig`.
 
+        Warning:
+            All keyword arguments are passed to `plt.plot`. If you pass an invalid keyword argument, you will get an error.
+
         Example:
             >>> codf = CoordinatesDataFrame.from_numpy(np.random.randint(0, 105, (1, 23, 2)))
             >>> codf.visualize_frame(0)
@@ -163,22 +180,34 @@ class CoordinatesDataFrame(SoccerTrackMixin, pd.DataFrame):
         .. image:: /_static/visualize_frame.png
         """
 
-        _marker_kwargs = dict(
-            marker="o",
-            markeredgecolor="None",
-            linestyle="None",
-            **(marker_kwargs) or {},
+        _marker_kwargs = merge_dicts(
+            {"marker": "o", "markeredgecolor": "None", "linestyle": "None"},
+            marker_kwargs,
         )
-        _ball_kwargs = dict(
-            _marker_kwargs, zorder=3, ms=6, markerfacecolor="w", **(ball_kwargs) or {}
+
+        _ball_kwargs = merge_dicts(
+            _marker_kwargs,
+            {"zorder": 3, "ms": 6, "markerfacecolor": "w"},
+            marker_kwargs,
+            ball_kwargs,
         )
-        _home_kwargs = dict(
-            _marker_kwargs, ms=10, markerfacecolor="b", **(home_kwargs) or {}
+        _home_kwargs = merge_dicts(
+            _marker_kwargs,
+            {"zorder": 10, "ms": 10, "markerfacecolor": "b"},
+            marker_kwargs,
+            home_kwargs,
         )
-        _away_kwargs = dict(
-            _marker_kwargs, ms=10, markerfacecolor="r", **(away_kwargs) or {}
+
+        _away_kwargs = merge_dicts(
+            _marker_kwargs,
+            {"zorder": 10, "ms": 10, "markerfacecolor": "r"},
+            marker_kwargs,
+            home_kwargs,
         )
-        _save_kwargs = dict(facecolor="black", pad_inches=0.0, **(save_kwargs) or {})
+
+        _save_kwargs = merge_dicts(
+            {"facecolor": "black", "pad_inches": 0.0}, save_kwargs
+        )
 
         _df = self.copy()
         _df = _df[_df.index == frame_idx]
@@ -228,16 +257,20 @@ class CoordinatesDataFrame(SoccerTrackMixin, pd.DataFrame):
         away_kwargs: Optional[dict[str, Any]] = None,
         save_kwargs: Optional[dict[str, Any]] = None,
     ):
-        """Visualize a single frame.
+        """Visualize multiple frames using matplotlib.animation.FuncAnimation.
 
         Visualizes the frames and generates a pitch animation. The `CoordinatesDataFrame` is expected to already have been normalized so that the pitch is 105x68, e.g. coordinates on the x-axis range from 0 to 105 and coordinates on the y-axis range from 0 to 68.
+
+        To customize the animation, you can pass keyword arguments to `matplotlib.animation.FuncAnimation`. For example, to change the frame rate, you can pass `fps=30` to `save_kwargs` by, e.g. `codf.visualize_frames("animation.gif", save_kwargs={"fps": 30})`. See the `matplotlib.animation.FuncAnimation` documentation for more information.
+
+        Similarly, you can pass keyword arguments to change the appearance of the markers. For example, to change the size of the markers, you can pass `ms=6` to `away_kwargs` by, e.g. `codf.visualize_frames("animation.gif", away_kwargs={"ms": 6})`. See the `matplotlib.pyplot.plot` documentation for more information.  Note that `marker_kwargs` will be used for all markers but will be overwritten by `ball_kwargs`, `home_kwargs` and `away_kwargs` if a dictionary with the same key is passed (later dictionaries take precedence).
 
         Args:
             frame_idx: Frame number.
             save_path: Path to save the image. Defaults to None.
-            ball_key: Key for the ball. Defaults to "ball".
-            home_key: Key for the home team. Defaults to "0".
-            away_key: Key for the away team. Defaults to "1".
+            ball_key: Key (TeamID) for the ball. Defaults to "ball".
+            home_key: Key (TeamID) for the home team. Defaults to "0".
+            away_key: Key (TeamID) for the away team. Defaults to "1".
             marker_kwargs: Keyword arguments for the markers.
             ball_kwargs: Keyword arguments specifically for the ball marker.
             home_kwargs: Keyword arguments specifically for the home team markers.
@@ -247,31 +280,53 @@ class CoordinatesDataFrame(SoccerTrackMixin, pd.DataFrame):
         Note:
             `marker_kwargs` will be used for all markers but will be overwritten by `ball_kwargs`, `home_kwargs` and `away_kwargs`. All keyword arguments are passed to `plt.plot`. `save_kwargs` are passed to `FuncAnimation.save`.
 
+        Warning:
+            All keyword arguments are passed either to `plt.plot` and `FuncAnimation.save`. If you pass an invalid keyword argument, you will get an error.
+
         Example:
             >>> codf = load_codf("/path/to/codf.csv")
             >>> codf.visualize_frames("/path/to/save.mp4")
+
+            # Heres a demo using random data
+            >>> codf = CoordinatesDataFrame.from_numpy(np.random.randint(0, 50, (1, 23, 2)))
+            >>> codf = codf.loc[codf.index.repeat(5)] # repeat the same frame 5 times
+            >>> codf += np.array([[0,1,2,3,4]]).T # add some movment
+            >>> codf.visualize_frames('_static/visualize_frames.gif', save_kwargs={'fps':2})
+
+        .. image:: _static/visualize_frames.gif
         """
-        _marker_kwargs = dict(
-            marker="o",
-            markeredgecolor="None",
-            linestyle="None",
-            **(marker_kwargs) or {},
+        _marker_kwargs = merge_dicts(
+            {"marker": "o", "markeredgecolor": "None", "linestyle": "None"},
+            marker_kwargs,
         )
-        _ball_kwargs = dict(
-            _marker_kwargs, zorder=3, ms=6, markerfacecolor="w", **(ball_kwargs) or {}
+
+        _ball_kwargs = merge_dicts(
+            _marker_kwargs,
+            {"zorder": 3, "ms": 6, "markerfacecolor": "w"},
+            marker_kwargs,
+            ball_kwargs,
         )
-        _home_kwargs = dict(
-            _marker_kwargs, ms=10, markerfacecolor="b", **(home_kwargs) or {}
+        _home_kwargs = merge_dicts(
+            _marker_kwargs,
+            {"zorder": 10, "ms": 10, "markerfacecolor": "b"},
+            marker_kwargs,
+            home_kwargs,
         )
-        _away_kwargs = dict(
-            _marker_kwargs, ms=10, markerfacecolor="r", **(away_kwargs) or {}
+
+        _away_kwargs = merge_dicts(
+            _marker_kwargs,
+            {"zorder": 10, "ms": 10, "markerfacecolor": "r"},
+            marker_kwargs,
+            home_kwargs,
         )
-        _save_kwargs = dict(
-            dpi=100,
-            fps=10,
-            extra_args=["-vcodec", "libx264", "-pix_fmt", "yuv420p"],
-            savefig_kwargs=dict(facecolor="black", pad_inches=0.0),
-            **(save_kwargs) or {},
+
+        _save_kwargs = merge_dicts(
+            {
+                "dpi": 100,
+                "fps": 10,
+                "savefig_kwargs": {"facecolor": "black", "pad_inches": 0.0},
+            },
+            save_kwargs,
         )
 
         _df = self.copy()
--- tests/test_dataframe/test_coordinatesdataframe.py
+++ tests/test_dataframe/test_coordinatesdataframe.py
@@ -29,6 +29,25 @@ class TestCoordinatesDataFrame(unittest.TestCase):
         codf.visualize_frame(0, save_path=save_path)
         assert save_path.exists(), f"File {save_path} does not exist"
 
+    def test_visualize_frame_with_custom_kwargs(self):
+        codf = load_codf(csv_path)
+        save_path = outputs_path / "test_visualize_frame_with_custom_save_kwargs.png"
+
+        # make sure the file does not exist or delete it if it does
+        if save_path.exists():
+            save_path.unlink()
+
+        marker_kwargs = {"markerfacecolor": "green", "ms": 30}
+        saved_kwargs = {"dpi": 300, "bbox_inches": "tight"}
+
+        codf.visualize_frame(
+            0,
+            save_path=save_path,
+            marker_kwargs=marker_kwargs,
+            save_kwargs=saved_kwargs,
+        )
+        assert save_path.exists(), f"File {save_path} does not exist"
+
     def test_visualize_frames(self):
         codf = load_codf(csv_path)
         save_path = outputs_path / "test_visualize_frames.mp4"
@@ -40,8 +59,21 @@ class TestCoordinatesDataFrame(unittest.TestCase):
         codf.visualize_frames(save_path=save_path)
         assert save_path.exists(), f"File {save_path} does not exist"
 
+    def test_visualize_frames_with_custom_save_kwargs(self):
+        codf = load_codf(csv_path)
+        save_path = outputs_path / "test_visualize_frames_with_custom_save_kwargs.mp4"
+
+        # make sure the file does not exist or delete it if it does
+        if save_path.exists():
+            save_path.unlink()
+
+        saved_kwargs = {"dpi": 300, "fps": 50}
+
+        codf.visualize_frames(save_path=save_path, save_kwargs=saved_kwargs)
+        assert save_path.exists(), f"File {save_path} does not exist"
+
     def test_numpy(self):
-        pass
+        pass # TODO
 
     def test_to_pitch_coordinates(self):
-        pass
+        pass # TODO
