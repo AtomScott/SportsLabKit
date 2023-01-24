0ec7fc964436a78e13b3afee481e2c00c65874f3
Atom Scott <atom.james.scott@gmail.com>
2023-01-24 12:07:10 +0900

    add test_from_dict_2

--- tests/test_dataframe/test_coordinatesdataframe.py
+++ tests/test_dataframe/test_coordinatesdataframe.py
@@ -114,5 +114,21 @@ class TestCoordinatesDataFrame(unittest.TestCase):
 
         assert codf.shape == (3, 10)
 
+    def test_from_dict_2(self):
+        d = {
+            "home_team": {
+                "player_1a": {1: (1, 2), 2: (3, 4), 3: (5, 6), 4: (5, 6), 5: (5, 6)},
+                "player_1b": {1: (7, 8), 2: (9, 10), 3: (11, 12)},
+            },
+            "away_team": {
+                "player_2a": {1: (13, 14), 2: (15, 16), 3: (17, 18)},
+                "player_2b": {1: (19, 20), 2: (21, 22), 3: (23, 24)},
+            },
+            "ball": {"ball": {3: (29, 30)}},
+        }
+        codf = CoordinatesDataFrame.from_dict(d)
+        print(codf)
+        assert codf.shape == (5, 10)
+
     def test_to_pitch_coordinates(self):
         pass  # TODO
