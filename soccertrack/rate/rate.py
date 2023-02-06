1b91313cc5274401536b3a4c7aa6cd18c616bdd0
IkumaUchida <ikuma.uchida@gmail.com>
2023-02-06 12:56:21 +0900

    fix grid_count

--- soccertrack/rate/rate.py
+++ soccertrack/rate/rate.py
@@ -4,15 +4,6 @@ import pandas as pd
 from soccertrack.dataframe import CoordinatesDataFrame
 from soccertrack.rate.agg_func import get_agg_func
 
-# setup initial variables
-
-grid_xmin = 0
-grid_ymin = 0
-grid_xmax = 105
-grid_ymax = 68
-window_x = 16
-window_y = 12
-
 
 def grid_count(ball_traj: np.ndarray) -> list[int]:
     """
@@ -30,6 +21,13 @@ def grid_count(ball_traj: np.ndarray) -> list[int]:
     Returns:
     moving_area_count(list[int]): A tuple of two lists, the first of which is a list of moving areas (each of which is a 2D numpy array) and the second of which is a corresponding list of counts of the number of elements in each moving area.
     """
+    # setup initial variables
+    grid_xmin = 0
+    grid_ymin = 0
+    grid_xmax = 105
+    grid_ymax = 68
+    window_x = 16
+    window_y = 12
 
     pitch_length_x = np.linspace(grid_xmin, grid_xmax, window_x + 1)
     pitch_length_y = np.linspace(grid_ymin, grid_ymax, window_y + 1)
@@ -45,14 +43,13 @@ def grid_count(ball_traj: np.ndarray) -> list[int]:
                 & (ball_traj[:, 1] <= pitch_length_y[idx_y])
             ]
             moving_area_count.append(len(moving_area))
-    return moving_area_count
+    return np.array(moving_area_count).reshape(window_x, window_y)
 
 
 # calulate xG
 def rate_xG(codf: CoordinatesDataFrame, agg_func="w_mean"):
     ball_traj = list(codf.iter_players())[-1][1].values
     moving_area_count = grid_count(ball_traj)
-    moving_area_count = np.array(moving_area_count).reshape(window_x, window_y)
 
     xg_score = get_agg_func(agg_func)(xg_mtx, moving_area_count)
     return xg_score
@@ -62,7 +59,6 @@ def rate_xG(codf: CoordinatesDataFrame, agg_func="w_mean"):
 def rate_xT(codf: CoordinatesDataFrame, agg_func="w_mean"):
     ball_traj = list(codf.iter_players())[-1][1].values
     moving_area_count = grid_count(ball_traj)
-    moving_area_count = np.array(moving_area_count).reshape(window_x, window_y)
 
     xt_score = get_agg_func(agg_func)(xt_mtx, moving_area_count)
     return xt_score
