ed42c294716128a2cdec3c7579ad03a5eee977a9
Atom Scott <atom.james.scott@gmail.com>
2023-06-28 00:53:21 +0900

    feat: Add soccer tracking types and classes.
    - Add `Detection` class and `Detections` class to `soccertrack/types`
    - Define methods for processing images and predictions in `Detections` class
    - Import relevant classes in `__init__.py`
    - Define `Box`, `Vector`, and `NpImage` types in `types.py` using numpy arrays

---
+++ soccertrack/types/__init__.py
@@ -0,0 +1,6 @@
+from soccertrack.types.types import _pathlike, Box, Vector
+from soccertrack.types.detection import Detection
+from soccertrack.types.detections import Detections
+from soccertrack.types.tracklet import Tracklet
+
+__all__ = ["_pathlike", "Box", "Vector", "Detection", "Detections", "Tracklet"]
