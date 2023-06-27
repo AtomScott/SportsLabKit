ced80c5da7f2c0d2ecc6e9f17e72bbcd3d88765b
Atom Scott <atom.james.scott@gmail.com>
2023-06-28 02:41:22 +0900

    chore: Upgrade package installations and dependencies
    - Upgraded package installation to editable installation
    - Added installation of `ultralytics` package
    - Added installation of `CLIP` package from GitHub repository

--- test_enviroment.sh
+++ test_enviroment.sh
@@ -2,9 +2,12 @@ conda create -y --name soccertrack-test-env python=3.10
 conda activate soccertrack-test-env
 
 python --version
-python -m pip install .
+python -m pip install -e .
 python -m pip install torch torchvision pytorch-lightning
 python -m pip install git+https://github.com/KaiyangZhou/deep-person-reid.git
+python -m pip install ultralytics
+python -m pip install git+https://github.com/openai/CLIP.git
+
 python -m pytest tests
 
 conda deactivate
