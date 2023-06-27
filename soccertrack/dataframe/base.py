3f5bd7cc98b949d329145368ab9533553cead4d4
Atom Scott <atom.james.scott@gmail.com>
2023-06-28 02:27:35 +0900

    feat: Refactor base dataframe class and methods for improved functionality
    - Add `get_data` method to `SoccerTrackMixin` class for selecting specific data based on frame, playerid, teamid and attributes in [soccertrack/dataframe/base.py]
    - Use single-line lambdas in `iter_frames` for improved readability and conciseness in [soccertrack/dataframe/base.py]

--- soccertrack/dataframe/base.py
+++ soccertrack/dataframe/base.py
@@ -24,6 +24,31 @@ class SoccerTrackMixin(object):
         else:
             self.to_csv(path_or_buf, mode="w")
 
+    def get_data(self, frame=None, playerid=None, teamid=None, attributes=None):
+        """Get specific data from the dataframe.
+
+        Args:
+            frame (int or list of int, optional): Frame(s) to get.
+            player (int or list of int, optional): Player ID(s) to get.
+            team (int or list of int, optional): Team ID(s) to get.
+            attributes (str or list of str, optional): Attribute(s) to get.
+
+        Returns:
+            pd.DataFrame: Dataframe with the selected data.
+        """
+        df = self
+
+        if frame is not None:
+            df = df.iloc[[frame]]
+        if playerid is not None:
+            df = df.xs(playerid, level="PlayerID", axis=1, drop_level=False)
+        if teamid is not None:
+            df = df.xs(teamid, level="TeamID", axis=1, drop_level=False)
+        if attributes is not None:
+            df = df.xs(attributes, level="Attributes", axis=1, drop_level=False)
+
+        return df
+
     def iter_frames(self, apply_func=None):
         """Iterate over the frames of the dataframe.
 
@@ -47,9 +72,7 @@ class SoccerTrackMixin(object):
             apply_func = lambda x: x
         for index, group in self.groupby(level=("TeamID", "PlayerID"), axis=1):
             if drop:
-                yield index, apply_func(
-                    group.droplevel(level=("TeamID", "PlayerID"), axis=1)
-                )
+                yield index, apply_func(group.droplevel(level=("TeamID", "PlayerID"), axis=1))
             else:
                 yield index, apply_func(group)
 
