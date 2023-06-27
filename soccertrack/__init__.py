93a1f841474607f2a91b2d8fdd36e79d35a97bbc
Atom Scott <atom.james.scott@gmail.com>
2023-06-28 02:21:59 +0900

    refactor: Export main tracking classes in soccertrack package.
    - Export important classes and functions as public API
    - Import necessary modules for detection and image handling
    - Omit large changes from diff summary for readability

--- soccertrack/__init__.py
+++ soccertrack/__init__.py
@@ -19,6 +19,19 @@ with all_logging_disabled(), warnings.catch_warnings():
     from vidgear.gears import WriteGear, CamGear
 
 import soccertrack.datasets  # noqa
+import soccertrack.detection_model
+import soccertrack.image_model
 from soccertrack.camera import Camera
 from soccertrack.dataframe import BBoxDataFrame, CoordinatesDataFrame
 from soccertrack.io import load_df
+from soccertrack.types.tracklet import Tracklet
+
+__all__ = [
+    "Camera",
+    "Tracklet",
+    "BBoxDataFrame",
+    "CoordinatesDataFrame",
+    "load_df",
+    "CamGear",
+    "WriteGear",
+]
