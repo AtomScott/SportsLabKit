692b322477cf185607c50fd62f8de7b5e28dc46e
Atom Scott <atom.james.scott@gmail.com>
2023-06-29 00:56:49 +0900

    feat: Implement teamtrack functionality

--- soccertrack/mot/__init__.py
+++ soccertrack/mot/__init__.py
@@ -2,5 +2,6 @@ from soccertrack.mot.base import MultiObjectTracker
 from soccertrack.mot.deepsort import DeepSORTTracker
 from soccertrack.mot.sort import SORTTracker
 from soccertrack.mot.bytetrack import BYTETracker
+from soccertrack.mot.teamtrack import TeamTracker
 
-__all__ = ["MultiObjectTracker", "SORTTracker", "DeepSORTTracker", "BYTETracker"]
+__all__ = ["MultiObjectTracker", "SORTTracker", "DeepSORTTracker", "BYTETracker", "TeamTracker"]
