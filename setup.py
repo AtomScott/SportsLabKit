e7bc441cb4cf30b7c2dc922a1f1ae44c7205ebd0
Atom Scott <atom.james.scott@gmail.com>
2023-08-15 16:58:30 +0900

    update setup files

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
-import os
 
 from setuptools import find_packages, setup
+
 
 with open("requirements.txt") as f:
     required = f.read().splitlines()
 
 
 setup(
-    name="soccertrack",
+    name="sportslabkit",
     packages=find_packages(),
     version="0.1.0",
     description="A short description of the project.",
