f789ee56c9cf2001f81af1d1a3a979256f17ef18
Atom Scott <atom.james.scott@gmail.com>
2023-08-31 01:04:08 +0900

    fix comment out mistake

--- sportslabkit/calibration_model/dummy.py
+++ sportslabkit/calibration_model/dummy.py
@@ -16,22 +16,6 @@ class DummyCalibrationModel(BaseCalibrationModel):
         else:
             return self.homographies[self.image_count]
         
-    def forward(self, x):
-        # Return the precomputed detections based on image_count
-        if self.input_is_batched:
-            start_index = self.image_count
-            end_index = self.image_count + len(x)
-            self.image_count += len(x)
-            if self.image_count >= len(self.homographies):
-                self.reset_image_count()
-            return self.homographies[start_index:end_index]
-        else:
-            homograhy = self.homographies[self.image_count]
-            self.image_count += 1
-            results = [homograhy]
-            self.reset_image_count()
-            return results
-
     def reset_image_count(self):
         self.image_count = 0
         logger.debug("Resetting image count")
