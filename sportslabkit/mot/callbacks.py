3ba8db7c4e72e4ca9cb35f5f7d8e1067bc9d4585
IkumaUchida <ikuma.uchida@gmail.com>
2023-09-06 18:13:33 +0900

    "Move Callbacks class into MOT directory for better organization

--- sportslabkit/mot/callbacks.py
+++ sportslabkit/mot/callbacks.py
@@ -10,55 +10,20 @@ Example:
             print("Training is starting")
 """
 
-from functools import wraps
 
 from scipy import stats
 from sportslabkit.logger import logger
 
 from sportslabkit.types import Vector
 from sportslabkit.vector_model import BaseVectorModel
-
-
-def with_callbacks(func):
-    """Decorator for wrapping methods that require callback invocations.
-
-    Args:
-        func (callable): The method to wrap.
-
-    Returns:
-        callable: The wrapped method.
-    """
-
-    @wraps(func)
-    def wrapper(self, *args, **kwargs):
-        event_name = func.__name__
-        self._invoke_callbacks(f"on_{event_name}_start")
-        result = func(self, *args, **kwargs)
-        self._invoke_callbacks(f"on_{event_name}_end")
-        return result
-
-    return wrapper
-
-
-class Callback:
-    """Base class for creating new callbacks.
-
-    This class defines the basic structure of a callback and allows for dynamic method creation
-    for handling different events in the Trainer's lifecycle.
-
-    Methods:
-        __getattr__(name: str) -> callable:
-            Returns a dynamically created method based on the given name.
-    """
-
-    pass
+from sportslabkit.mot.base import MultiObjectTracker, Callback
 
 
 class TeamClassificationCallback(Callback):
     def __init__(self, vector_model: BaseVectorModel):
         """
         Initialize TeamClassificationCallback.
-        
+
         Args:
             vector_model (BaseVectorModel): A trained object responsible for classifying teams.
                             This object is generally loaded from a pickle file that
@@ -73,7 +38,7 @@ class TeamClassificationCallback(Callback):
                 - Example: If you're using an SVM-based classifier saved using pickle, this `predict`
                         method would take a feature vector and output the corresponding team IDs
                         (either 0 or 1 in a 2-class problem).
-        
+
         Note:
             The `vector_model` is expected to be a serialized object (e.g., pickle file)
             conforming to the above `predict` method specifications. It's commonly generated
@@ -82,7 +47,7 @@ class TeamClassificationCallback(Callback):
         super().__init__()
         self.vector_model = vector_model
 
-    def on_track_sequence_end(self, tracker) -> None:
+    def on_track_sequence_end(self, tracker: MultiObjectTracker) -> None:
         """
         Method called at the end of a track sequence. During this phase, team classification
         is performed on each tracklet using the `vector_model.predict`.
@@ -105,7 +70,7 @@ class TeamClassificationCallback(Callback):
             tracklet_features: Vector = tracklet.get_observations("feature")
 
             # Using forward method for model inference
-            predicted_team_id = self.vector_model.predict(tracklet_features)
+            predicted_team_id = self.vector_model(tracklet_features)
 
             # Assuming you want the most frequent prediction as the final team ID
             most_frequent_team_id = stats.mode(predicted_team_id).mode
