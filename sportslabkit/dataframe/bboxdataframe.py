fec8def634e0c549b2f5e5e2f658cfe5551fb28b
Atom Scott <atom.james.scott@gmail.com>
2023-09-06 22:21:15 +0900

    wip decouple bboxdataframe

--- sportslabkit/dataframe/bboxdataframe.py
+++ sportslabkit/dataframe/bboxdataframe.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 import uuid
 from collections.abc import Iterable
-from hashlib import md5
 from pathlib import Path
 from typing import Any
 
@@ -10,35 +9,15 @@ import cv2
 import numpy as np
 import pandas as pd
 
+from sportslabkit.types import _COLOR_NAMES, Rect
 from sportslabkit.utils import make_video
+from sportslabkit.visualization import add_bbox_to_frame, add_frame_id_to_frame
 
 from ..logger import logger
 from ..utils import MovieIterator, get_fps
 from .base import BaseSLKDataFrame
 from .coordinatesdataframe import CoordinatesDataFrame
 
-
-# https://clrs.cc/
-_COLOR_NAME_TO_RGB = {
-    "navy": ((0, 38, 63), (119, 193, 250)),
-    "blue": ((0, 120, 210), (173, 220, 252)),
-    "aqua": ((115, 221, 252), (0, 76, 100)),
-    "teal": ((15, 205, 202), (0, 0, 0)),
-    "olive": ((52, 153, 114), (25, 58, 45)),
-    "green": ((0, 204, 84), (15, 64, 31)),
-    "lime": ((1, 255, 127), (0, 102, 53)),
-    "yellow": ((255, 216, 70), (103, 87, 28)),
-    "orange": ((255, 125, 57), (104, 48, 19)),
-    "red": ((255, 47, 65), (131, 0, 17)),
-    "maroon": ((135, 13, 75), (239, 117, 173)),
-    "fuchsia": ((246, 0, 184), (103, 0, 78)),
-    "purple": ((179, 17, 193), (241, 167, 244)),
-    "black": ((24, 24, 24), (220, 220, 220)),
-    "gray": ((168, 168, 168), (0, 0, 0)),
-    "silver": ((220, 220, 220), (0, 0, 0)),
-}
-
-_COLOR_NAMES = list(_COLOR_NAME_TO_RGB)
 
 # global index to access bbox information for each frame
 X_INDEX = 0  # xmin
@@ -105,10 +84,10 @@ class BBoxDataFrame(BaseSLKDataFrame):
         return BBoxDataFrame
 
     def visualize_frame(
-        self: BBoxDataFrame,
+        self,
         frame_idx: int,
         frame: np.ndarray,
-        draw_frame_id: bool = False,
+        visualizer:'Visualizer' | None = None,
     ) -> np.ndarray:
         """Visualize the bounding box of the specified frame.
 
@@ -116,10 +95,17 @@ class BBoxDataFrame(BaseSLKDataFrame):
             self (BBoxDataFrame): BBoxDataFrame object.
             frame_idx (int): Frame ID.
             frame (np.ndarray): Frame image.
-            draw_frame_id (bool, optional): Whether to draw the frame ID. Defaults to False.
         Returns:
             frame(np.ndarray): Frame image with bounding box.
         """
+
+        if visualizer is None:
+            visualizer = DefaultVisualizer(self)
+
+        frame_df = self.loc[self.index == frame_idx]
+        drawn_frame = visualizer.draw_bbdf(frame_df, frame)
+        return drawn_frame
+
         if frame_idx not in self.index:
             return frame
         frame_df = self.loc[self.index == frame_idx]
@@ -137,6 +123,10 @@ class BBoxDataFrame(BaseSLKDataFrame):
             player_df.loc[frame_idx, ["bb_left", "bb_top", "bb_width", "bb_height"]]
 
             x1, y1, w, h = player_df.loc[frame_idx, ["bb_left", "bb_top", "bb_width", "bb_height"]].values.astype(int)
+            Rect(x1, y1, w, h)
+
+
+
             x2, y2 = x1 + w, y1 + h
 
             label = f"{team_id}_{player_id}"
@@ -578,141 +568,3 @@ class BBoxDataFrame(BaseSLKDataFrame):
         return BBoxDataFrame(df)
 
 
-def add_bbox_to_frame(
-    image: np.ndarray,
-    left: int,
-    top: int,
-    right: int,
-    bottom: int,
-    label: str | None = None,
-    color: str | None = None,
-) -> np.ndarray:
-    """Add bounding box and label to image.
-
-    Args:
-        img (np.ndarray): Image.
-        left (int): Bounding box left coordinate.
-        top (int): Bounding box top coordinate.
-        right (int): Bounding box right coordinate.
-        bottom (int): Bounding box bottom coordinate.
-        label (str): Label.
-        color (str): Color.
-    Returns:
-        img (np.ndarray): Image with bounding box and label.
-    """
-    _DEFAULT_COLOR_NAME = "purple"
-
-    if isinstance(image, np.ndarray) is False:
-        raise TypeError("'image' parameter must be a numpy.ndarray")
-    try:
-        left, top, right, bottom = int(left), int(top), int(right), int(bottom)
-    except ValueError as e:
-        raise TypeError("'left', 'top', 'right' & 'bottom' must be a number") from e
-
-    if label and isinstance(label, str) is False:
-        raise TypeError("'label' must be a str")
-
-    if label and not color:
-        hex_digest = md5(label.encode()).hexdigest()
-        color_index = int(hex_digest, 16) % len(_COLOR_NAME_TO_RGB)
-        color = _COLOR_NAMES[color_index]
-
-    if not color:
-        color = _DEFAULT_COLOR_NAME
-
-    if isinstance(color, str) is False:
-        raise TypeError("'color' must be a str")
-
-    if color not in _COLOR_NAME_TO_RGB:
-        msg = "'color' must be one of " + ", ".join(_COLOR_NAME_TO_RGB)
-        raise ValueError(msg)
-
-    colors = [_rgb_to_bgr(item) for item in _COLOR_NAME_TO_RGB[color]]
-    color_value, _ = colors
-
-    image = cv2.rectangle(image, (left, top), (right, bottom), color_value, 2)
-
-    if label:
-        _, image_width, _ = image.shape
-        fontface = cv2.FONT_HERSHEY_TRIPLEX
-        fontscale = 0.5
-        thickness = 1
-
-        (label_width, label_height), _ = cv2.getTextSize(label, fontface, fontscale, thickness)
-        rectangle_height, rectangle_width = 1 + label_height, 1 + label_width
-
-        rectangle_bottom = top
-        rectangle_left = max(0, min(left - 1, image_width - rectangle_width))
-
-        rectangle_top = rectangle_bottom - rectangle_height
-        rectangle_right = rectangle_left + rectangle_width
-
-        label_top = rectangle_top + 1
-
-        if rectangle_top < 0:
-            rectangle_top = top
-            rectangle_bottom = rectangle_top + label_height + 1
-
-            label_top = rectangle_top
-
-        label_left = rectangle_left + 1
-        label_bottom = label_top + label_height
-
-        rec_left_top = (rectangle_left, rectangle_top)
-        rec_right_bottom = (rectangle_right, rectangle_bottom)
-
-        cv2.rectangle(image, rec_left_top, rec_right_bottom, color_value, -1)
-
-        cv2.putText(
-            image,
-            text=label,
-            org=(label_left, int(label_bottom)),
-            fontFace=fontface,
-            fontScale=fontscale,
-            color=(0, 0, 0),
-            thickness=thickness,
-            lineType=cv2.LINE_4,
-        )
-    return image
-
-
-def add_frame_id_to_frame(image: np.ndarray, frame_id: int) -> np.ndarray:
-    """Add frame id to image.
-
-    Args:
-        img (np.ndarray): Image.
-        frame_id (int): Frame id.
-    Returns:
-        img (np.ndarray): Image with frame id.
-    """
-    if isinstance(image, np.ndarray) is False:
-        raise TypeError("'image' parameter must be a numpy.ndarray")
-
-    try:
-        frame_id = int(frame_id)
-    except ValueError as e:
-        raise TypeError("'frame_id' must be a number") from e
-
-    fontface = cv2.FONT_HERSHEY_TRIPLEX
-    fontscale = 5
-    thickness = 1
-
-    label = f"frame: {frame_id}"
-
-    # draw frame id on the top left corner
-    (label_width, label_height), _ = cv2.getTextSize(label, fontface, fontscale, thickness)
-
-    label_left = 10
-    label_bottom = label_height + 10
-
-    cv2.putText(
-        image,
-        text=label,
-        org=(label_left, int(label_bottom)),
-        fontFace=fontface,
-        fontScale=fontscale,
-        color=(0, 0, 0),
-        thickness=thickness,
-        lineType=cv2.LINE_4,
-    )
-    return image
