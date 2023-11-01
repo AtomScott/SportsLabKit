8e971ccaff52472f529cd882dd907c7515067e42
shunsuke-iwashita <129936839+shunsuke-iwashita@users.noreply.github.com>
2023-11-01 18:34:02 +0900

    Update fld.py

--- sportslabkit/calibration_model/fld.py
+++ sportslabkit/calibration_model/fld.py
@@ -53,7 +53,8 @@ class SimpleContourCalibrator(BaseCalibrationModel):
         hull = cv2.convexHull(contour)
         return hull
     
-    def _get_first_point(self, hull):
+    def _get_left_top_point(self, hull):
+        """Find the nearest point from the upper left corner."""
         sorted_hull = sorted(hull, key=lambda x:x[0][0]*x[0][0] + x[0][1]*x[0][1])
         return sorted_hull[0][0]
 
