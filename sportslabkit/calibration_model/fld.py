f7d58eb9db887f02dfd4751a7e107b038d14a587
AtomScott <AtomScott@users.noreply.github.com>
2023-11-01 10:16:55 +0000

    style fixes by ruff

--- sportslabkit/calibration_model/fld.py
+++ sportslabkit/calibration_model/fld.py
@@ -52,7 +52,7 @@ class SimpleContourCalibrator(BaseCalibrationModel):
     def _approximate_hull(self, contour):
         hull = cv2.convexHull(contour)
         return hull
-    
+
     def _get_upper_left_courner(self, hull):
         """Find the nearest point from the upper left corner."""
         sorted_hull = sorted(hull, key=lambda x:x[0][0]*x[0][0] + x[0][1]*x[0][1])
