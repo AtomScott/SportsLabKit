3623811752fbae5422b7fc8305fbd1489aee0fcc
AtomScott <AtomScott@users.noreply.github.com>
2023-11-01 01:30:15 +0000

    style fixes by ruff

--- sportslabkit/calibration_model/fld.py
+++ sportslabkit/calibration_model/fld.py
@@ -52,7 +52,7 @@ class SimpleContourCalibrator(BaseCalibrationModel):
     def _approximate_hull(self, contour):
         hull = cv2.convexHull(contour)
         return hull
-    
+
     def _farthest_point_from(self, reference_point, point_list):
         """Find the point in 'point_list' that is farthest from 'reference_point'."""
         max_dist = 0
@@ -148,14 +148,14 @@ class SimpleContourCalibrator(BaseCalibrationModel):
 class FLDCalibrator(SimpleContourCalibrator):
     def __init__(self, length_threshold=50, distance_threshold=50, canny_th1=50, canny_th2=150, canny_aperture_size=3, do_merge=True, dst_points=None):
         """Initialize the line-based calibrator with given parameters."""
-        self.fld = cv2.ximgproc.createFastLineDetector(_length_threshold=self.length_threshold, 
-                                                       _distance_threshold=self.distance_threshold, 
-                                                       _canny_th1=self.canny_th1, _canny_th2=self.canny_th2, 
+        self.fld = cv2.ximgproc.createFastLineDetector(_length_threshold=self.length_threshold,
+                                                       _distance_threshold=self.distance_threshold,
+                                                       _canny_th1=self.canny_th1, _canny_th2=self.canny_th2,
                                                        _canny_aperture_size=self.canny_aperture_size, _do_merge=self.do_merge)
         if dst_points is None:
             # Using the dimensions of a standard soccer pitch (105m x 68m)
             self.dst_points = np.array([[0, 0], [105, 0], [105, 68], [0, 68]], dtype=np.float32)
-            
+
     def _preprocess_image(self, image):
         """Convert the image to grayscale and apply thresholding and morphological operations."""
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -167,7 +167,7 @@ class FLDCalibrator(SimpleContourCalibrator):
 
     def _get_lines(self, image):
         """Detect lines in the image using Fast Line Detector."""
-        
+
         lines = self.fld.detect(image)
         return lines
 
